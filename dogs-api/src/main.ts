e70be3705df858be6e29748898dc78dda44ea044
CaioCesarPS <caio_cesar_ps@hotmail.com>
2025-10-05 01:20:33 -0300

    feat: update CORS configuration to allow requests from the frontend URL

--- dogs-api/src/main.ts
+++ dogs-api/src/main.ts
@@ -5,7 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://dogs-frontend.caiocps-homelab.com.br',
+    ],
   });
 
   app.useGlobalPipes(new ValidationPipe());
